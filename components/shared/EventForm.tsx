66cb3436be0fb69126059d22749a69941203480e
Phillip Lu <Plu2978@conestogac.on.ca>
2024-02-11 19:02:24 -0500

    fixed async error

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -24,6 +24,7 @@ import Image from "next/image"
 import DatePicker from "react-datepicker";
 import { Checkbox } from "../ui/checkbox";
 import { useUploadThing } from '@/lib/uploadthing'
+import { useRouter } from "next/navigation"
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -34,6 +35,7 @@ type EventFormProps = {
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([])
   const initialValues = eventDefaultValues;
+  const router = useRouter();
 
   const { startUpload } = useUploadThing('imageUploader')
 
@@ -44,13 +46,34 @@ const EventForm = ({ userId, type }: EventFormProps) => {
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof eventFormSchema>) {
-        const eventData = values;
-
+      async function onSubmit(values: z.infer<typeof eventFormSchema>) {
         let uploadedImageUrl = values.imageUrl;
 
         if (files.length > 0) {
           const uploadedImages = await startUpload(files)
+
+          if(!uploadedImages) {
+            return
+          }
+
+          uploadedImageUrl = uploadedImages[0].url
+        }
+
+        if (type === 'Create') {
+          try {
+            // const newEvent = await createEvent({
+            //   event: { ...values, imageUrl: uploadedImageUrl },
+            //   userId,
+            //   path: 'profile'
+            // })
+
+            // if (newEvent) {
+            //   form.reset();
+            //   router.push(`/events/${newEvent._id}`)
+            // }
+          } catch (error) {
+            console.log(error)
+          }
         }
       }
   
