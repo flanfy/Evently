0345d4773518ab364ecae91f179e9df3b0824154
Phillip Lu <Plu2978@conestogac.on.ca>
2024-02-11 20:28:23 -0500

    finished event creation

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -27,6 +27,7 @@ import { useUploadThing } from '@/lib/uploadthing'
 import { useRouter } from "next/navigation"
 
 import "react-datepicker/dist/react-datepicker.css";
+import { createEvent } from "@/lib/actions/event.actions"
 
 type EventFormProps = {
     userId: string
@@ -61,16 +62,16 @@ const EventForm = ({ userId, type }: EventFormProps) => {
 
         if (type === 'Create') {
           try {
-            // const newEvent = await createEvent({
-            //   event: { ...values, imageUrl: uploadedImageUrl },
-            //   userId,
-            //   path: 'profile'
-            // })
+            const newEvent = await createEvent({
+              event: { ...values, imageUrl: uploadedImageUrl },
+              userId,
+              path: 'profile'
+            })
 
-            // if (newEvent) {
-            //   form.reset();
-            //   router.push(`/events/${newEvent._id}`)
-            // }
+            if (newEvent) {
+              form.reset();
+              router.push(`/events/${newEvent._id}`)
+            }
           } catch (error) {
             console.log(error)
           }
